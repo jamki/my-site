cafc9bf33dad9751b1de91797366effe0d4eac3a
James Kimpton <jamesk@potatolondon.com>
2018-10-29 22:24:02 +0000

    Content update

---
+++ src/components/Intro.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="jk">
+    <h1>James Kimpton</h1>
+    <h2>Front End Engineer</h2>
+
+    <h3>About</h3>
+    <p>I'm a curious front end engineer working at <a href="https://p.ota.to/" target="_blank" rel="noopener">potato</a> with over 5 years commercial experience working at London Digital Agencies. I'm always eager to learn and improve my skillset and strive to keep up to date with the latest front end trends, tools and workflows.</p>
+
+    <h3>What I do</h3>
+    <p>Working closely with design, UX and back end developers to deliver an improved web experience. Making sure that code quality is high by encouraging QA within the development team and emphasizing the importance of code reviews.</p>
+
+    <h3>Skillset</h3>
+    <ul>
+      <li>Experience writing HTML5 to a high semantic standard with a11y and localization in mind alongside browser feature support</li>
+      <li>Writing performant ES6 JavaScript, Vue.js and Angular.js</li>
+      <li>Build tools like gulp, grunt and webpack with a babel transpiler</li>
+      <li>Using SCSS/LESS, encouraging the use of BEM and utility classes where appropriate</li>
+      <li>Responsive design, always encouraging the mobile first approach</li>
+      <li>Integration with RESTful APIs to deliver a fluid user experience</li>
+      <li>Experience with Unit Testing such as Jasmine and Karma</li>
+      <li>Leveraging frameworks like Material UI to rapidly prototype</li>
+      <li>Versioning with GIT and tracking with Jira, Gitlab and Codebase</li>
+      <li>Project planning and estimating features</li>
+      <li>Understanding state management using Vuex for SPAs</li>
+      <li>Taking apart Sketch or Photoshop files to create an "pixel perfect" site</li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      name: 'James Kimpton',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
